e94af9b3c47acf58878e4dde4f1d68c7ea008e93
copygodistaken <eric@33ad.org>
2020-07-24 12:36:58 -0500

    Chapter 15 - update to add files (oops)

---
+++ app/errors/old_handlers.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app, db
+
+@bp.app.errorhandler(404)
+def not_found_error(error):
+	return render_template('/errors/templates/404.html'), 404
+
+@bp.app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('/errors/templates/500.html'), 500
+
+
+
